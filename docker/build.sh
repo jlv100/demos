4d190af18a1dde0bdb3b79b2d09cd5be3e372c44
Van Hemert, John <john.vanhemert@pioneer.com>
2018-04-26 10:41:36 -0500

    cleaned up old data files and folders.  added minidb to gitignore

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,3 @@
 
 docker build -t demos/miniconda2 miniconda2
 docker build -t demos/jupyter jupyter
-docker build -t demos/caffe caffe
-docker build -t demos/caffe2 caffe2
--- start_notebooks.sh
+++ start_notebooks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 PORT=${1:-8888}
 OTHER=${2:-""}
 echo "Running notebooks listening on port $PORT..."
@@ -11,8 +13,7 @@ docker run \
 --name demo_notebooks \
 -p $PORT:8888 \
 -v $PWD/notebooks:/notebooks \
-$OTHER demos/caffe2
+$OTHER demos/jupyter
 
-sleep 5
-
-docker logs --tail 100 demo_notebooks | grep -A 1 -B 1 'to login with a token'
+echo "Run the following to get the jupyter token:"
+echo "docker logs demo_notebooks"
