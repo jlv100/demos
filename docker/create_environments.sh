8caeb428cdfddd301fa53b8221991aa806be4d83
Van Hemert, John <john.vanhemert@pioneer.com>
2020-03-16 17:08:45 -0500

    make sure to cd into home, which is tmp to create environments

--- docker/create_environments.sh
+++ docker/create_environments.sh
@@ -1,6 +1,8 @@
 
 echo "Creating environments.  This may take some time..."
-ls -1 /environments/*.yml | while read YAML
+cd
+cp /environments/*.yml ./
+ls -1 *.yml | while read YAML
 do
     echo "Creating $YAML..."
     conda env create -f $YAML
