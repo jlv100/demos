69a59a4292cec5552736bedaf179792921f20582
Van Hemert, John <john.vanhemert@pioneer.com>
2020-03-16 17:11:31 -0500

    force a pull when running from dockerhub

--- start_jupyterlab_dockerhub.sh
+++ start_jupyterlab_dockerhub.sh
@@ -4,10 +4,16 @@ set -e
 set -x
 NVIDIA=`which nvidia-smi > /dev/null && echo "--runtime=nvidia" || echo ""`
 
+docker pull jlv100/demos
+
+mkdir -p $MYSCR
+
 docker run \
 $NVIDIA \
 --user $UID:`id -g` \
 --rm \
 --network host \
 -v $PWD/notebooks:/notebooks \
+-v $MYSCR:/tmp \
 jlv100/demos
+
